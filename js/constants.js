acea9f18762cc2964c2555b3cebf0234a79a56e8
Alex Masterov <alex.masterow@gmail.com>
2019-07-07 13:41:10 +0300

    Remove the scary constant

--- js/constants.js
+++ js/constants.js
@@ -4,21 +4,10 @@
   var ImageScale = {
     STEP: 25,
     MIN: 25,
-    MAX: 100
-  };
-
-  var SliderData = {
-    LINE_WIDTH: 453,
-    PIN_WIDTH: 18,
-    RANGE: 435,
-    Position: {
-      MIN: 9,
-      MAX: 444
-    }
+    MAX: 100,
   };
 
   window.Constants = {
     ImageScale: ImageScale,
-    SliderData: SliderData
   };
 })();
