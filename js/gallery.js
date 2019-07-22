f43d6415b37e9888fef3151372f955dd63585699
Alex Masterov <alex.masterow@gmail.com>
2019-07-22 14:45:43 +0300

    Simplify Gallery
    Use `dataset` + `index`

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,21 @@
 'use strict';
 
-(function (EventUtil, FilterBar, Filter, Review) {
-  var debounce = EventUtil.debounce;
+(function (
+  debounce,
+  isEnterKey,
+  FilterBar,
+  Filter,
+  Review,
+) {
   var imageList = document.querySelector('.pictures');
-  var imageTemplate = document.querySelector('#picture')
-    .content
-    .querySelector('.picture');
+  var imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-  var renderImage = function (image) {
+  var renderImage = function (image, id) {
     var node = imageTemplate.cloneNode(true);
     node.querySelector('.picture__img').src = image.url;
-    node.querySelector('.picture__comments').textContent = image.comments.length;
     node.querySelector('.picture__likes').textContent = image.likes;
+    node.querySelector('.picture__comments').textContent = image.comments.length;
+    node.dataset.id = id;
 
     return node;
   };
@@ -22,44 +26,38 @@
 
   var setImages = function (images) {
     var fragment = document.createDocumentFragment();
-    images.forEach(function (image) {
-      fragment.appendChild(renderImage(image));
+    images.forEach(function (image, index) {
+      fragment.appendChild(renderImage(image, index));
     });
 
     imageList.querySelectorAll('.picture').forEach(removeImage);
     imageList.appendChild(fragment);
   };
 
-  var setImageSrc = function (image) {
-    return image.src.slice(34);
-  };
-
-  var isImageClass = function (image) {
-    return image && (image.className === 'picture__img');
+  var isPicture = function (element) {
+    return element.classList.contains('picture');
   };
 
   var Gallery = function () {
     this._images = [];
-    this._urlToImages = {};
     this._onFilterChange = debounce(this._onFilterChange.bind(this));
     this._onImagesClick = this._onImagesClick.bind(this);
     this._onImageEnterPress = this._onImageEnterPress.bind(this);
 
     this._filterBar = new FilterBar(this._onFilterChange);
     this._review = new Review();
+
+    this._addEventListeners();
   };
 
   Gallery.prototype.add = function (images) {
     this._images = this._images.concat(images);
-
-    this._images.forEach(function (image) {
-      this._urlToImages[image.url] = image;
-    }.bind(this));
-
     this._filterBar.show();
     setImages(this._images);
+  };
 
-    this._addEventListeners();
+  Gallery.prototype.preview = function (id) {
+    this._review.show(this._images[id]);
   };
 
   Gallery.prototype._addEventListeners = function () {
@@ -67,41 +65,27 @@
     imageList.addEventListener('keydown', this._onImageEnterPress);
   };
 
-  Gallery.prototype._makeImageDict = function (images) {
-    images.forEach(function (image) {
-      this._urlToImages[image.url] = image;
-    }.bind(this));
-  };
-
   Gallery.prototype._onFilterChange = function (filter) {
     setImages(Filter[filter](this._images));
   };
 
   Gallery.prototype._onImagesClick = function (evt) {
-    if (isImageClass(evt.target)) {
-      var src = setImageSrc(evt.target);
-    } else {
-      return;
-    }
-
-    this._review.open(this._urlToImages[src]);
+    var element = evt.target.parentElement;
+    return isPicture(element)
+      && this.preview(element.dataset.id);
   };
 
   Gallery.prototype._onImageEnterPress = function (evt) {
-    if (isImageClass(evt.target.children[0]) && EventUtil.isEnterKey(evt)) {
-      var src = setImageSrc(evt.target.children[0]);
-    } else {
-      return;
-    }
-
-    this._review.open(this._urlToImages[src]);
+    return isEnterKey(evt)
+      && isPicture(evt.target)
+      && this.preview(evt.target.dataset.id);
   };
 
   window.Gallery = Gallery;
-
 })(
-    window.EventUtil,
-    window.FilterBar,
-    window.Filter,
-    window.Review
+  window.EventUtil.debounce,
+  window.EventUtil.isEnterKey,
+  window.FilterBar,
+  window.Filter,
+  window.Review
 );
